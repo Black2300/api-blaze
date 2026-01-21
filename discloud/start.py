e49674fffc39d4d4743263883fb093d22634c447
Black2300 <134557314+Black2300@users.noreply.github.com>
2026-01-21 02:23:43 -0300

    Update start.py

--- discloud/start.py
+++ discloud/start.py
@@ -1,5 +1,8 @@
-import os
 import uvicorn
 
-port = int(os.environ.get("PORT", "8080"))
-uvicorn.run("api:app", host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    uvicorn.run(
+        "api:app",
+        host="0.0.0.0",
+        port=8080
+    )
